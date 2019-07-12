3a2f73f4878f02642ed287bad4faacd386f1f0bb
Nilakshi <nilakshi.ma1509@ict.gnu.ac.in>
2019-07-12 12:23:46 +0530

    6th task

--- src/main/java/com/stackroute/demo/Main.java
+++ src/main/java/com/stackroute/demo/Main.java
@@ -7,10 +7,12 @@ public class Main
 {
     public static void main(String[] args) {
 
+        //added the postprocessor class and defined its bean in the configuration file
         //using ConfigurableApplicationContext to manually close the context
         ConfigurableApplicationContext configurableApplicationContext = new ClassPathXmlApplicationContext("beans.xml");
         //getting the bean
         BeanLifecycleDemoBean beanLifecycleDemoBean = configurableApplicationContext.getBean("beanLifecycle",BeanLifecycleDemoBean.class);
+        //all the postProcess methods are called too
         //closing the context so that destroy methods are called
         configurableApplicationContext.close();
     }
