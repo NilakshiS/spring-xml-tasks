e8ef3b1fe3b43737ce719d91f06e07e6bd5da01e
Nilakshi <nilakshi.ma1509@ict.gnu.ac.in>
2019-07-19 10:37:40 +0530

    added comments

--- src/main/java/com/stackroute/demo/BeanLifecycleDemoBean.java
+++ src/main/java/com/stackroute/demo/BeanLifecycleDemoBean.java
@@ -4,20 +4,25 @@ import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
 public class BeanLifecycleDemoBean implements InitializingBean, DisposableBean {
+
+    //method of DisposableBean interface
     @Override
-    public void destroy() throws Exception {
+    public void destroy() {
         System.out.println("destroy method called before bean is destroyed");
     }
 
+    //method of InitializingBean interface
     @Override
     public void afterPropertiesSet() throws Exception {
         System.out.println("after properties set method called before bean is initialized and after properties set");
     }
 
+    //custom init method
     public void customInit(){
         System.out.println("Custom init called");
     }
 
+    //custom destroy method
     public void customDestroy(){
         System.out.println("Custom destroy called");
     }
